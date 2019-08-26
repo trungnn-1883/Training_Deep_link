f3fda2380e937d047adc8605564f24140ccb8b07
ngoc.trung <nguyen.ngoc.trungc@sun-asterisk.com>
2019-08-26 10:15:31 +0700

    first init

---
+++ app/src/main/java/gooner/demo/training_deep_link/MainActivity.kt
@@ -0,0 +1,12 @@
+package gooner.demo.training_deep_link
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/test/java/gooner/demo/training_deep_link/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package gooner.demo.training_deep_link
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
